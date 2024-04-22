c2047d691e4826a3f27249c8e901054fea52f027
Yakub <abdulaev.yakub@list.ru>
2024-04-22 11:38:11 +0300

    link with css

--- registration/settings.py
+++ registration/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,10 @@ SECRET_KEY = 'django-insecure-h()^^1u#@6qh$&9fr8*8ocuj1%0%xqg5*#0*-sb=&6if1(aee3
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 ALLOWED_HOSTS = []
 
@@ -116,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
